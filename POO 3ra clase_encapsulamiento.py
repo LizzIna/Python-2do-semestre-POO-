9dc012d46971ee940add2dfcfa295ad07a5ed593
LizzIna <valentinaantonia.calderon@alumnos.ulagos.cl>
2024-10-07 11:05:08 -0300

    cambio de nombre de archivo

---
+++ POO 3ra clase_encapsulamiento.py
@@ -0,0 +1,14 @@
+class Test:
+    def __uno(self):
+        print("Este metodo es privado, ya que su nombre comienza por __")
+
+    def dos(self):
+        print("Este metodo es publico")
+
+    def __tres__(self):
+        print("Este metodo tambien es publico, porque su nombre empieza y acaba por __")
+
+p = Test()
+p.__uno() #No funcina porque es privado
+p.dos()
+p.__tres__()
